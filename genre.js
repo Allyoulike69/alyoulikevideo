fde5ba1a80fc995809093fb1e3602322b655fa03
Allyoulike69 <sonyhatakekojiro0002@gmail.com>
2026-06-04 16:18:35 +0700

    Update genre.js

--- genre.js
+++ genre.js
@@ -1,10 +1,10 @@
 // ==================== KONFIGURASI ====================
-const DATA_URL = "https://allyoulike69.github.io/alyoulikevideo/p/daftar.json";
-const BASE_URL = "https://allyoulike69.github.io/alyoulikevideo/p/";
-const HOME_URL = "https://allyoulike69.github.io/alyoulikevideo/";
+const DATA_URL = "https://alyoulikevideo.pages.dev/p/daftar.json";
+const BASE_URL = "https://alyoulikevideo.pages.dev/p/";
+const HOME_URL = "https://alyoulikevideo.pages.dev/";
 const COMIC_URL = "https://allyoulikecomic.neocities.org/";
 const VIDEO34_URL = "https://www.google.com";
-const SEARCH_PAGE_URL = "https://allyoulike69.github.io/alyoulikevideo/search";
+const SEARCH_PAGE_URL = "https://alyoulikevideo.pages.dev/search";
 
 // ==================== VARIABEL GLOBAL ====================
 let allVideos = [];
