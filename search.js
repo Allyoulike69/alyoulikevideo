1cf39a8a1e6d71b25a1c61ccb5c3515dbf126a12
Allyoulike69 <sonyhatakekojiro0002@gmail.com>
2026-06-07 15:12:15 +0700

    Update search.js

--- search.js
+++ search.js
@@ -4,7 +4,7 @@
 const DATA_URL = "https://alyoulikevideo.pages.dev/p/daftar.json";
 const BASE_URL = "https://alyoulikevideo.pages.dev/p/";
 const HOME_URL = "https://alyoulikevideo.pages.dev/";
-const COMIC_URL = "https://allyoulikecomic.neocities.org/";
+const COMIC_URL = "https://allyoulikecomic.pages.dev/";
 const VIDEO34_URL = "https://alyoulikevideo.pages.dev/comingsoon.html";
 const ITEMS_PER_PAGE = 24;
 
